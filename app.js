8e36b3b7d30d2cacdf73e4378442bd6a9beb6798
Jacqueline Lim <jacweizhen6679@gmail.com>
2023-10-15 16:49:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ function getWeather() {
   let temperature = document.getElementById("temperature");
   let description = document.getElementById("description");
   let location = document.getElementById("location");
-  
+
+  let api = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "2da1e54d97209acb6696623d0a65fa9e";
-  let api = "https://api.openweathermap.org/data/2.5/weather?q={city name}&APPID={apiKey}";
 
   location.innerHTML = "Locating...";
 
   navigator.geolocation.getCurrentPosition(success, error);
 
   function success(position) {
+    //get exact coordinates
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
@@ -22,6 +23,7 @@ function getWeather() {
       longitude +
       "&appid=" +
       apiKey +
+      //set to metric
       "&units=metric";
 
     fetch(url)
@@ -29,15 +31,19 @@ function getWeather() {
       .then(data => {
         console.log(data);
         let temp = data.main.temp;
+        //get temperature in celsius
         temperature.innerHTML = temp + "° C";
+        //get location by latitude & longitude
         location.innerHTML =
           data.name + " (" + latitude + "°, " + longitude + "°)";
+        //put description
         description.innerHTML = data.weather[0].main;
       });
   }
-
+//place an error message
   function error() {
-    location.innerHTML = "Error encountered when retreiving your location";
+    location.innerHTML = "Error when retrieving your location";
   }
-//call the function getWeather
+}
+//call the function 
 getWeather();
